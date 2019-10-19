860a1fdab128549c9e2dedf0b7353295ce08664b
BPitts8019 <bradleyj.pitts@gmail.com>
2019-10-18 17:32:32 -0700

    Adding tasks complete

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, {useState, useContext} from "react";
+
+//context
+import TodoListContext from "../contexts/TodoList.js";
 
 function TodoForm () {
+   const {dispatch} = useContext(TodoListContext);
+   const [taskName, setTaskName] = useState("");
+
+   const handleChange = event => {
+      setTaskName(event.target.value);
+   };
+
+   const handleClick = event => {
+         event.preventDefault();
+         
+         // check for errors
+         if (taskName === "") {
+            alert("Please enter a task");
+            return;
+         }
+
+         //reset the form
+         const item = taskName;
+         setTaskName("");
+
+         //add new task
+         dispatch({
+            type: "ADD_TASK", 
+            payload: {
+               item,
+               id: new Date(),
+               completed: false
+            }
+         });
+   };
+
    return (
-      <form>
-         <input type="text" />
-         <button>Add Task</button>
+      <form onSubmit={handleClick}>
+         <input className="newTask" type="text" placeholder="...todo" onChange={handleChange} value={taskName} />
+         <button type="submit">Add Task</button>
       </form>
    );
 }
--- src/reducers/TodoList.js
+++ src/reducers/TodoList.js
@@ -6,6 +6,11 @@ export const initialState = [{
 
 export function reducer (state, action) {
    switch (action.type) {
+      case "ADD_TASK":
+         return [
+            ...state,
+            action.payload
+         ];
       default:
          return state;
    }
